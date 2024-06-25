6f02ec111f0abfa818996cc747d5e5fac619acf3
Hugo Chen <66862008+hugoingg@users.noreply.github.com>
2024-06-25 23:38:44 +0800

    Update multiple_diseases_pred.py

--- multiple_diseases_pred.py
+++ multiple_diseases_pred.py
@@ -10,17 +10,12 @@ st.set_page_config(page_title="Diagnose AI",
                    page_icon="🧑‍⚕️")
 
 # Get the directory where this script is located
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Define paths relative to the current directory
-diabetes_model_path = os.path.join(current_dir, 'saved models', 'diabetes_model.sav')
-heart_model_path = os.path.join(current_dir, '..', 'saved models', 'heart_model.sav')
-parkinsons_model_path = os.path.join(current_dir, '..', 'saved models', 'parkinsons_model.sav')
+working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # loading the saved models
-diabetes_model = pickle.load(open(diabetes_model_path, 'rb'))
-heart_disease_model = pickle.load(open('../saved models/heart_model.sav','rb'))
-parkinsons_model = pickle.load(open('../saved models/parkinsons_model.sav','rb'))
+diabetes_model = pickle.load(open(f'{working_dir}/saved models/diabetes_model.sav','rb'))
+heart_disease_model = pickle.load(open(f'{working_dir}/saved models/heart_model.sav','rb'))
+parkinsons_model = pickle.load(open(f'{working_dir}/saved models/parkinsons_model.sav','rb'))
 
 # sidebar for navigation
 with st.sidebar:
